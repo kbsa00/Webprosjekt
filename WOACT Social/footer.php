dc40fda1937ed075dcfcfde8c006d9c1429fd374
Sebastian <seb.knutsen@gmail.com>
2017-05-26 17:09:58 +0200

    FOOTLINK JA

--- WOACT Social/footer.php
+++ WOACT Social/footer.php
@@ -4,10 +4,10 @@
             <a href="Forside.php">
                 <img id="wdalslogo" src="Bilder/Logo/wdals-full-logo-hvit.png">
             </a>
-            <a href="Omoss.php">
+            <a href="Omoss.php" class="foot-link">
                 <li>Om oss</li>
             </a>
-            <a href="login.php">
+            <a href="login.php" class="foot-link">
                 <li>Logg inn</li>
             </a>
         </div>
